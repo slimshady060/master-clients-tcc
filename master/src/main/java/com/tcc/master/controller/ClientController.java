ff5bedd0a6acf89b32d50f0f5c11b0a724ed785b
John ROdriguez <slimshady060@hotmail.com>
2023-07-06 00:10:05 -0500

    add controller

---
+++ master/src/main/java/com/tcc/master/controller/ClientController.java
@@ -0,0 +1,30 @@
+package com.tcc.master.controller;
+
+import com.tcc.master.model.Client;
+import com.tcc.master.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clients")
+@CrossOrigin(origins = "http://localhost:4200")
+public class ClientController {
+    private final ClientService clientService;
+
+    @Autowired
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Client>> getAllClients() {
+        List<Client> clientList = clientService.getAllClients();
+        return ResponseEntity.ok(clientList);
+    }
+}
--- master/src/main/java/com/tcc/master/model/Client.java
+++ master/src/main/java/com/tcc/master/model/Client.java
@@ -1,18 +1,22 @@
 package com.tcc.master.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "clients")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name")
-    private String Name;
+    private String name;
 
     @Column(name = "email")
     private String email;
